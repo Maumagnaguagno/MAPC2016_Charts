99f3721e3b526be8fc2e9fe1d4b06f18d95fed21
Mau Magnaguagno <maumagnaguagno@gmail.com>
2019-05-18 15:04:53 -0300

    Convert CSV data as needed

--- analyze.rb
+++ analyze.rb
@@ -1,23 +1,23 @@
 begin
   # Use all datasets if none given
   (ARGV.empty? ? Dir.glob('*/*.csv') : ARGV).each {|dataset|
-    csv = IO.read(dataset).split("\n").map! {|row| row.split(',')}
-    1.upto(1001) {|row| csv[row].map! {|i| i.to_i}}
+    csv = IO.binread(dataset).split("\n")
+    _, team_a, team_b = csv.shift.split(',')
     dominion_a = dominion_b = dominion_draw = 0
     # Skip setup step
-    2.upto(1001) {|row|
+    1.upto(1000) {|row|
+      row = csv[row].split(',')
       # Team dominance
-      case csv[row][1] <=> csv[row][2]
+      case row[1].to_i <=> row[2].to_i
       when 1 then dominion_a += 1
       when -1 then dominion_b += 1
       else dominion_draw += 1
       end
     }
     # Output
-    team_a = csv[0][1]
-    team_b = csv[0][2]
-    money_a = csv[1001][1]
-    money_b = csv[1001][2]
+    row = csv[1000].split(',')
+    money_a = row[1].to_i
+    money_b = row[2].to_i
     puts "#{team_a} vs #{team_b} - simulation #{dataset[/sim(\d+)of3/,1]} of 3".center(50, '_'),
          sprintf('%14s made %12d$ (%.2f%%)', team_a, money_a, money_a.fdiv(money_a.abs + money_b.abs) * 100),
          sprintf('%14s made %12d$ (%.2f%%)', team_b, money_b, money_b.fdiv(money_a.abs + money_b.abs) * 100),
